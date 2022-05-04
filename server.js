bdfb2959fc0ea45dd34ee5a6da317a3f46c54b61
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-04 19:51:34 -0400

    set up utils and server

--- server.js
+++ server.js
@@ -2,7 +2,21 @@ const path = require("path");
 const express = require("express");
 const session = require("express-session");
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create({});
+const sequelize = require("./config/config")
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+    secret: "Super secret secret",
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize
+    })
+  };
+  app.use(session(sess));
+
+  const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,6 +34,3 @@ app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
   sequelize.sync({ force: false });
 });
-
-
-
