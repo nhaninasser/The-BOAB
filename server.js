8c3b31d1377c9b010bfc3fcb0722ab7a7598053c
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-05 17:35:06 -0400

    added js and tried to link all the handlebars

--- server.js
+++ server.js
@@ -1,38 +1,43 @@
-const path = require("path");
+const sequelize = require("./config/config");
 const express = require("express");
-const session = require("express-session");
+const routes = require("./controllers");
+const helpers = require("./utils/helper")
 const exphbs = require("express-handlebars");
-
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const sequelize = require("./config/config")
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const hbs = exphbs.create({ helpers });
+const path = require("path");
+const session = require("express-session");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-    secret: "Super secret secret",
+    secret: "SUPER SECRET SECRET",
     cookie: {},
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
-        db: sequelize
-    })
-};
+      db: sequelize,
+    }),
+  };
+
+const app = express();
+
+
+
+const PORT = process.env.PORT || 3001;
+
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(session(sess));
 
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(routes);
 
-app.listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({ force: false });
-});
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}!`));
+}); 
