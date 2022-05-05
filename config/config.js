9cbac804efadbdde360b965817a87023497319c5
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-05 19:04:24 -0400

    tried to fix the errors

--- config/config.js
+++ config/config.js
@@ -8,7 +8,8 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      port: 3306,
+      socketPath: '/var/run/mysqld/mysqld.sock'
     });
 
 module.exports = sequelize;
