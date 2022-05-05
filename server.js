690ae1ddf9053a29f53ea7f9f71f536a03f690ce
Nasser Nasser <nhani.nasser@gmail.com>
2022-05-05 18:14:08 -0400

    fixed the breaks

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session(sess));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.use(routes);
 
